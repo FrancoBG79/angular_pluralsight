65c00516bb056fddc1e1058ca92db3087250e22a
FrancoBG79 <fbg2468@gmail.com>
2026-06-24 09:28:06 +0000

    add app from pluralsight

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ProductSelection } from './products/product-selection/product-selection';
+import { ReviewSearch } from './reviews/review-search/review-search';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductSelection, ReviewSearch],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('upskill');
+  protected title = 'Acme Product Management';
 }
