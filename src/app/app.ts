ee59e55ffb049a0c1e506f014a71e32c9d3ee457
FrancoBG79 <fbg2468@gmail.com>
2026-06-30 05:32:45 +0000

    signals start

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { ProductSelection } from './products/product-selection/product-selection';
-import { ReviewSearch } from './reviews/review-search/review-search';
+import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
+// import { ProductSelection } from './products/product-selection/product-selection';
+// import { ReviewSearch } from './reviews/review-search/review-search';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductSelection, ReviewSearch],
+  imports: [
+    // ProductSelection, 
+    // ReviewSearch,
+    RouterLinkActive, 
+    RouterLink, 
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'Acme Product Management';
+  pageTitle = 'Acme Product Management';
+
+  cartCount = 0;
 }
