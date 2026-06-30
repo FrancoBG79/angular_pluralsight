8820c4371dcd7cb86a63a259abfbcf0b0176e3de
FrancoBG79 <fbg2468@gmail.com>
2026-06-30 07:31:42 +0000

    started

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,8 +1,13 @@
 /* Defines the product */
+import { Review } from "../reviews/review";
 export interface Product {
   id: number;
   productName: string;
+  productCode: string;
   description: string;
   price: number;
   supplierIds?: number[];
+  quantityInStock?: number;
+  hasReviews?: boolean;
+  reviews?: Review[];
 }
