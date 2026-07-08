260f03b128236d3e04b2bf57053a33408235b823
FrancoBG79 <fbg2468@gmail.com>
2026-07-08 09:27:33 +0000

    nav fix to standalone

--- src/app/animations/site-header/site-header.component.ts
+++ src/app/animations/site-header/site-header.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {
+    RouterLinkActive,
+    RouterLink, 
+    RouterOutlet 
+  } from '@angular/router';
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css'],
+  imports: [
+    RouterLinkActive,
+    RouterLink, 
+  ]
 })
 export class SiteHeaderComponent {
   constructor() {}
