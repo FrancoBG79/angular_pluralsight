bad80635eefddef35154b74f928f12dbd41b54a7
FrancoBG79 <fbg2468@gmail.com>
2026-07-08 12:39:17 +0000

    change catalog to signal

--- src/app/animations/catalog/catalog.component.ts
+++ src/app/animations/catalog/catalog.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { IProduct } from './product.model';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
 import { ProductDetailsComponent } from "../product-details/product-details.component";
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'bot-catalog',
@@ -12,37 +13,32 @@ import { ProductDetailsComponent } from "../product-details/product-details.comp
   imports: [RouterLink, RouterLinkActive, ProductDetailsComponent],
 })
 export class CatalogComponent {
-  products: IProduct[] = [];
-  filter: string = '';
+  private cartSvc: CartService = inject(CartService);
+  private productSvc: ProductService = inject(ProductService);
+  private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  
 
   filterLinks = ['Heads', 'Arms', 'Torsos', 'Bases', 'All'];
-
-  constructor(
-    private cartSvc: CartService,
-    private productSvc: ProductService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) { }
+  filterBy = signal('All');
+  products = toSignal(this.productSvc.getProducts(), { initialValue: [] });
+  filteredProducts = computed(() => 
+      this.filterBy() === 'All'
+        ? this.products()
+        : this.products().filter(
+          (product: any) => product.category === this.filterBy()
+        )
+  )
+  
 
   ngOnInit() {
-    this.productSvc.getProducts().subscribe((products) => {
-      this.products = products;
-    });
     this.route.queryParams.subscribe((params) => {
-      this.filter = params['filter'] ?? '';
+      this.filterBy.set(params['filter'] ?? 'All');
     })
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
     this.router.navigate(['/cart']);
-  }
-
-  getFilteredProducts() {
-    return this.filter === 'All'
-      ? this.products
-      : this.products.filter(
-        (product: any) => product.category === this.filter
-      );
   }
 }
